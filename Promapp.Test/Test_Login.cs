97de3e5ab03f4e3cd81c5c66b6b726aeba3b1787
OEM <OEM@OEM-PC>
2019-03-20 21:04:33 +1300

    Update Test_Login.cs

--- Promapp.Test/Test_Login.cs
+++ Promapp.Test/Test_Login.cs
@@ -5,6 +5,8 @@ using Newtonsoft.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Tests
 {
@@ -21,16 +23,43 @@ namespace Tests
         public void Test1_Login_Success()
         {
             var ret = login.Login("fanzhang", "123", "Team1", true) as JsonResult;
-            dynamic jsonCollection = ret.Value;
-            Console.WriteLine(ret.Value);
+            var obj = ret.Value as object;
 
+            Assert.IsTrue((bool)obj.GetType().GetProperty("success").GetValue(obj));
         }
 
         [Test]
         public void Test2_Login_Failure()
         {
-            Assert.Pass();
+            object obj;
+            JsonResult ret;
+
+            var list = new List<List<object>>
+            {
+                new List<object> { "", "123", "Team1", true, false, "Username is null" },
+                new List<object> { "fanzhang", "", "Team2", true, false, "Password is null"},
+                new List<object> { "fanzhang", "123", "", true, false, "TEAM is null"},
+                new List<object> { "fanzhan", "123", "Team1", true, false, "username does not exist"},
+                new List<object> { "fanzhang", "12", "Team1", true, false, "password does not match"},
+                new List<object> { "fanzhang", "123", "Team2", true, false, "team does not match"},
+            };
+
+            foreach (var l in list)
+            {
+                ret = login.Login((string)l[0], (string)l[1], (string)l[2], (bool)l[3]) as JsonResult;
+                obj = ret.Value as object;
+                Assert.AreEqual((bool)obj.GetType().GetProperty("success").GetValue(obj), (bool)l[4]);
+                Assert.AreEqual((string)obj.GetType().GetProperty("message").GetValue(obj), (string)l[5]);
+            }
         }
 
+        [Test]
+        public void Test2_Teams()
+        {
+            var ret = login.Teams() as OkObjectResult;
+            object dValue = ret.Value as object;
+
+            Assert.AreNotEqual(dValue, null);
+        }
     }
 }
